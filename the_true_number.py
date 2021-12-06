39f5146c4f175e59636912359a8b677a61f1884e
wlkstack <martin.couturiaux@outlook.com>
2021-12-06 13:05:49 +0100

    ajout d'une fonction de comptage pour les essais

--- the_true_number.py
+++ the_true_number.py
@@ -3,14 +3,16 @@
 import random
 import sys
 
-# mise en place de la variable random
+# mise en place des différentes variables
 
 random_number = random.randint(1,100)
 saisie1 = input
+user_input = 0
 
 # $----------------------debut du programme---------------------------$
 
 while not saisie1 == random_number:
+    user_input = user_input + 1
     saisie1 = input("Saisissez un nombre entre 1 et 100 : ")
     try: 
         saisie1 = int(saisie1)
@@ -23,4 +25,5 @@ while not saisie1 == random_number:
         print("Le nombre random est plus petit")
     else:
         print("Gagné ! Vous avez trouvé le nombre correspondant")
+        print("Vous avez fais",user_input,"essais")
         sys.exit()
